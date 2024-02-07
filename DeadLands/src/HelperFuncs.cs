07ce214d51b66c6a1e58a1fe46f543e3c2d669cf
needleye <needleye@gmx.com>
2024-02-06 19:39:28 -0500

    Ready release files
    - Deadlands folder with current files
    - Current Deadlands creatures folder

--- DeadLands/src/HelperFuncs.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Deadlands;
-
-internal static class HelperFuncs
-{
-    public static Vector2 Abs(this Vector2 vec)
-        => new(Mathf.Abs(vec.x), Mathf.Abs(vec.y));
-}
