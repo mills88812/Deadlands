0049ac9ac816ba067bf02b6cb02d735f7694f195
Gary Lafortune <mom-puter@hotmail.com>
2024-07-02 12:36:03 -0500

    Adds a build script to generate a symlink automatically
    This should make it easier in general for new devs (or new people learning how to compile) to develop better.

---
+++ src/symlinkHandler.bat
@@ -0,0 +1,26 @@
+REM First step: find Rainworld's folder
+set progfiles=C:/Program Files
+set progfiles86=C:/Program Files (x86)
+set rainworld=Rain World/RainWorld_Data/StreamingAssets
+if "%DeadlandsLocation%"=="" (
+	echo "'DeadLandsLocation' not defined, automatically determining Rainworld's location..."
+	if EXIST "%progfiles%/Steam/steamapps/common/%rainworld%" (
+		set DeadlandsLocation="%progfiles%/Steam/steamapps/common/%rainworld%/mods/Deadlands"
+	) else if EXIST "%progfiles86%/Steam/steamapps/common/%rainworld%" (
+		set DeadlandsLocation="%progfiles86%/Steam/steamapps/common/%rainworld%/mods/Deadlands"
+	) else if EXIST "D:/Steam/steamapps/common/%rainworld%" (
+		set DeadlandsLocation="D:/Steam/steamapps/common/%rainworld%/mods/Deadlands"
+	) else (
+		echo "Unable to find the location of Deadland's mod folder within Rainworld!'"
+		exit /b 1
+	)
+)
+REM Second: Determine if Deadlands is already there; make a symlink if it isn't
+if EXIST %DeadLandsLocation% (
+	echo "Symbolic link seemingly already exists! Cool c:"
+) else (
+	echo "Generating symbolic link for Deadlands repo..."
+	echo %DeadLandsLocation%
+	echo "%cd%"
+	mklink /d %DeadLandsLocation% "%cd%/../mod"
+)
